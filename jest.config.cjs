580c39439610c2e90c667bf45f3fe921136c35f4
Logi E <logi.eyjolfsson@gmail.com>
2023-06-06 14:08:50 +0000

    npm->yarn in README, fixed test relative import bug

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,4 +3,8 @@ module.exports = {
     transform: { "^.+\\.(t|j)sx?$": "@swc/jest" },
     testEnvironment: "node",
     verbose: true,
+    // This is needed to find relative imports in src/
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
 };
